f504682ab8507c57736fa463b08809ab25914bb1
Steven Whitehead <swilliamwhitehead@gmail.com>
2025-09-09 10:42:47 -0400

    created xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Steven Whitehead on 9/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
